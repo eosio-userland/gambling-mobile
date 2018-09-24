a5f081cb03f64233e8222c7c23d348febd0a5682
Manson Li <lijinsong@rouchi.com>
2018-09-25 00:05:11 +0800

    add HomePage and Rules
    Signed-off-by: Manson Li <lijinsong@rouchi.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/views/Index'
+import HomePage from '@/views/HomePage'
+import Rules from '@/views/Rules'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', name: 'Index', component: Index }
+    { path: '/', name: 'Index', component: Index },
+    { path: '/home', name: 'HomePage', component: HomePage },
+    { path: '/rules', name: 'Rules', component: Rules }
   ]
 })
---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home-wrapper"></div>
+</template>
+<style lang="scss" scoped>
+  @import '~@/assets/styles/_var.scss';
+  .home-wrapper {
+    width: 100%;
+    height: px2rem(133.4);
+    background: url('~@/assets/images/game/index/index.png') no-repeat;
+    background-size: 100% 100%;
+  }
+</style>
