cc9f9697753a09c2a0c4dc79231c2374cef5433c
Akenokoru <Akenokoru.pan@gmail.com>
2018-09-24 15:45:50 +0800

    init index page

---
+++ src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+		<div class="index-wrapper"></div>  
+</template>
+<style lang="scss" scoped>
+	@import '~@/assets/styles/_var.scss';
+	.index-wrapper {
+		width: 100%;
+		height: px2rem(133.4);
+		background: url('~@/assets/images/game/index/index.png') no-repeat;
+		background-size: 100% 100%;
+	}
+</style>
