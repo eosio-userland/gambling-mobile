0f01f528805fe057db87b4cfb4d471e27fee3359
Akenokoru <Akenokoru.pan@gmail.com>
2018-09-24 23:45:10 +0800

    fix eslint error

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,12 @@
 <template>
-		<div class="index-wrapper"></div>  
+<div class="index-wrapper"></div>
 </template>
 <style lang="scss" scoped>
-	@import '~@/assets/styles/_var.scss';
-	.index-wrapper {
-		width: 100%;
-		height: px2rem(133.4);
-		background: url('~@/assets/images/game/index/index.png') no-repeat;
-		background-size: 100% 100%;
-	}
+@import '~@/assets/styles/_var.scss';
+.index-wrapper{
+width: 100%;
+height: px2rem(2088);
+background: url('~@/assets/images/game/index/index.png') no-repeat;
+background-size: 100% 100%;
+}
 </style>
