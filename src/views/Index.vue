c47b39e63543d2abe9befd048a469035339b5c6d
Manson Li <lijinsong@rouchi.com>
2018-09-26 20:00:44 +0800

    tmp
    Signed-off-by: Manson Li <lijinsong@rouchi.com>

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,17 @@
 <template>
 <div class="index-wrapper">
-  <div class="title-box" ref="title">
+  <div class="title-box">
     <img class="title-img" src="~@/assets/images/game/tmp/index_title.png">
   </div>
 
   <!--介绍-->
-  <div >
+  <div class="introduction">
+    <img class="introduction-img" src="~@/assets/images/game/tmp/introduction_bk.png">
   </div>
 
   <!--进入游戏-->
-  <div>
+  <div class="game-entry">
+    <img class="game-entry-img" src="~@/assets/images/game/tmp/game_entry.png">
   </div>
 </div>
 </template>
@@ -21,11 +23,27 @@ height: px2rem(2088);
 background: url('~@/assets/images/game/index/index.png') no-repeat;
 background-size: 100% 100%;
   .title-box{
-    width: px2rem(400);
+    width: 100%;
     height: px2rem(400);
     .title-img{
-      width: px2rem(200);
-      height: px2rem(200);
+      position: relative;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+    }
+  }
+  .introduction {
+    width: 100%;
+    height: px2rem(1288);
+  }
+  .game-entry{
+    width: 100%;
+    height: px2rem(400);
+    .game-entry-img{
+      position: relative;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
     }
   }
 }
