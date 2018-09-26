85b76363b07c7a875bc1863a047c3a7a8b690281
Manson Li <lijinsong@rouchi.com>
2018-09-26 17:58:29 +0800

    tmp
    Signed-off-by: Manson Li <lijinsong@rouchi.com>

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,17 @@
 <template>
-<div class="index-wrapper"></div>
+<div class="index-wrapper">
+  <div class="title-box" ref="title">
+    <img class="title-img" src="~@/assets/images/game/tmp/index_title.png">
+  </div>
+
+  <!--介绍-->
+  <div >
+  </div>
+
+  <!--进入游戏-->
+  <div>
+  </div>
+</div>
 </template>
 <style lang="scss" scoped>
 @import '~@/assets/styles/_var.scss';
@@ -8,5 +20,13 @@ width: 100%;
 height: px2rem(2088);
 background: url('~@/assets/images/game/index/index.png') no-repeat;
 background-size: 100% 100%;
+  .title-box{
+    width: px2rem(400);
+    height: px2rem(400);
+    .title-img{
+      width: px2rem(200);
+      height: px2rem(200);
+    }
+  }
 }
 </style>
